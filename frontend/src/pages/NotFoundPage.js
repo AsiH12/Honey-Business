a1989c07bc126dcacb843e4e6e9f12823ce8dd1e
Asi Hassan <asi.hassan111@gmail.com>
2024-09-03 14:28:24 +0300

    404 page and redirect button

--- frontend/src/pages/NotFoundPage.js
+++ frontend/src/pages/NotFoundPage.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom"; // For redirecting
 import "./NotFoundPage.css"; // Import the CSS file
 
 function NotFoundPage() {
+  const navigate = useNavigate();
+
   return (
     <div className="not-found-page">
-      <h1>אופס! העמוד אינו נמצא!</h1>
-      <p>נסו לחזור בחזרה</p>
+      <Typography
+        variant="h1"
+        className="not-found-title"
+      >
+        אופס! העמוד אינו נמצא!
+      </Typography>
+      <Button
+        variant="contained"
+        onClick={() => navigate('/')}
+        className="redirect-button"
+      >
+        נסו לחזור בחזרה
+      </Button>
     </div>
   );
 }
